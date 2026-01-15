5fa596aacf5a60c44e72e12e09bfd522c759ec25
AlexPerse10 <spuknitfire@gmail.com>
2026-01-15 23:50:59 +0100

    ejercicio manejo television poo

---
+++ programacion5_poo_ejercicios/src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        televisor tv1 = new televisor();
+        televisor tv2 = new televisor();
+
+        tv1.apagar();
+        tv1.subircanal();
+        tv1.subirvolumen();
+        tv1.subirvolumen();
+        tv1.subirvolumen();
+
+        System.out.println();
+
+        tv2.apagar();
+        tv2.bajarcanal();
+        tv2.subircanal();
+        tv2.bajarvolumen();
+        }
+    }
---
+++ programacion5_poo_ejercicios/src/main/java/org/example/televisor.java
@@ -0,0 +1,65 @@
+package org.example;
+
+public class televisor {
+    // clase televisor es la caja como asi decirlo, ahora tengo que guardas los atributos, los datos
+    // los datos internos son privados y necesita guardar su estado
+
+    private int canal;
+    private int volumen;
+    private boolean encendido;
+    // encendido es si o no por lo tanto boolean.
+
+    // canal, volumen y encendido guardan el estado actual
+    // el estado actual es decir por defecto se coloca en el constructor
+    public televisor() {
+        canal=1;
+        volumen=5;
+        encendido= false;
+    }
+
+    public void encender() { // esto son los metodos, es decir las acciones que puede realizar el televisor
+        encendido = true;
+        System.out.println("televisor encendido");
+    }
+    public void apagar() {
+        encendido = false;
+        System.out.println("el televisor esta apagado");
+    }
+
+    public void subircanal() {
+        if (encendido && canal<99){
+            canal++;
+            System.out.println("canal" +canal);
+        }
+
+    }
+    public void bajarcanal() {
+        if (encendido && canal>1){
+            canal--;
+            System.out.println("el canal es: "+canal);
+        }
+    }
+    public void subirvolumen() {
+        if (encendido && volumen<99){
+            volumen++;
+            System.out.println("volumen es: "+volumen);
+        }
+    }
+    public void bajarvolumen() {
+        if (encendido && volumen>1){
+            volumen--;
+            System.out.println("volumne es: "+volumen);
+        }
+    }
+
+    public int getCanal() {
+        return canal;
+    }
+    public int getVolumen() {
+        return volumen;
+    }
+    public boolean isEncendido() {
+        return encendido;
+    }
+
+}
