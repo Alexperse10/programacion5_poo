5937a20f01fb1114efdfa63d400f33d1c9b0682c
AlexPerse10 <spuknitfire@gmail.com>
2026-01-22 14:03:54 +0100

    se comienza con el ejercicio de paciente repaso

---
+++ programacion5_poo_ejercicios/src/main/java/org/example/paciente_poo_nuevo.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class paciente_poo_nuevo {
+
+    private static char def_sexo = 'x';
+    private String nombre;
+    private int edad;
+    private String Dni;
+    private char sexo;
+    private double peso;
+    private double altura;
+
+    public paciente_poo_nuevo(String dni,String nombre, int edad, char sexo, double peso, double altura) {
+        this.Dni = dni;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = sexo;
+        this.peso = peso;
+        this.altura = altura;
+
+    public paciente_poo_nuevo (String nombre, int edad, char sexo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this,sexo = sexo;
+        this.Dni = ;
+        }
+
+
+
+    }
+}
